7f63f92fae0dddfa9b0e983e827a610b75b777cb
tanvir-dsi <tanvir.dsi@bkash.com>
2021-05-01 22:12:22 +0600

    Add title layout and navigation menu in base layout

--- src/component/BaseLayout.js
+++ src/component/BaseLayout.js
@@ -1,15 +1,23 @@
 import {Route, Link ,Switch} from "react-router-dom";
 import PageNotFound from './PageNotFound';
 import NewStoryLayout from './NewStories';
+import '../../src/assets/style/main.css';
 function  BasicLayout() {
     return (
-        <div >
+            <div className='background'>
+                <div className='title'>Hacker News</div>
+                <div className='base-layout-item-div'>
+                    <div className='nav-div'>
+                        <div className='fragment-layout' style={{marginRight: 7}} >
+                            <Link  className='fragment-text' to="/">New</Link>
+                        </div>
+                        <div className='fragment-layout' >
+                            <Link className='fragment-text' to="/past">Past</Link>
+                        </div>
+                    </div>
 
-            <div>
-                <div className='nav-header'>Hacker News</div>
-                <div ><Link to="/" className="link">new</Link></div>
-                <div > <Link to="/past" className="link"><a>past</a></Link></div>
-            </div>
+                </div>
+
             <main>
                 <Switch>
                     <Route path="/" component={NewStoryLayout} exact />
